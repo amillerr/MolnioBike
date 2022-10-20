6cacf37a5f4ec8937c7cf3e0ed3294e032d03611
Tom Miller <62253468+amillerr@users.noreply.github.com>
2022-10-20 11:54:22 +0300

    Initial Commit

---
+++ MolnioBike/MolnioBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolnioBikeApp.swift
+//  MolnioBike
+//
+//  Created by Artem Axenov on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct MolnioBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
